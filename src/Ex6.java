270ce67a6b7263eb4fc390cf0cf2a76c443846a2
SamuelMello79 <mello.developer797@gmail.com>
2025-07-12 22:09:34 -0300

    [FIX] corrigido apenas alguns errinhos de retorno

--- src/Ex6.java
+++ src/Ex6.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Ex6 {
     public static void main(String[] args) {
-        // 6. Digite um valor e retorne o dobro, o triplo e a raiz quadrada
+        // 6. O usuário deve inserir um valor, e o programa deverá retornar o dobro, o triplo e a raiz quadrada
+        // deste valor.
         int numero, dobro, triplo;
         double raiz;
         Scanner scan = new Scanner(System.in);
@@ -13,7 +14,7 @@ public class Ex6 {
         raiz = Math.sqrt(numero);
         System.out.printf("O dobro de %d vale %d\n", numero, dobro);
         System.out.printf("O triplo de %d vale %d\n", numero, triplo);
-        System.out.printf("A raiz quadrada de %d vale %f\n", numero, raiz);
+        System.out.printf("A raiz quadrada de %d vale %.2f\n", numero, raiz); // .2f para arredondar as casas
 
     }
 }
